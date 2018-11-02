6cdf890145a5c3f21f370f010bfd18ea290b226f
Luca Franceschini <luca.franceschini92@gmail.com>
2018-11-02 11:28:52 +0100

    use eps rather than epsilon...

--- Open/Node.js-RV/fs/one/spec.pl
+++ Open/Node.js-RV/fs/one/spec.pl
@@ -12,6 +12,6 @@ match(Json, myfilter) :- cb_pre(Json, _, _, _, _).
 match(Json, myfilter) :- func_pre(Json, NameString, _, _, _),
                                  string_chars(NameString, ['f','s','.'|_]).
 
-trace_expression(fs_one, T) :- T = myfilter >> (epsilon \/ (open:cb:O)),
-                               O = (write:cb:O) \/ (close:cb:epsilon),
+trace_expression(fs_one, T) :- T = myfilter >> (eps \/ (open:cb:O)),
+                               O = (write:cb:O) \/ (close:cb:eps),
 	                           numbervars(T, 0, _).
