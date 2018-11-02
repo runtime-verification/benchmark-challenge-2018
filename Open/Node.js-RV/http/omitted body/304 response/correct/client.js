2128f8bb1ea8d01ccb2e6a7024126f51827a30d0
davideancona <davide.ancona@unige.it>
2018-11-02 17:10:31 +0100

    http benchmark

---
+++ Open/Node.js-RV/http/omitted body/304 response/correct/client.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/*
+Class http.ServerResponse
+method response.write(chunk[, encoding][, callback])
+
+Note that in the http module, the response body is omitted when the request is a HEAD request. Similarly, the 204 and 304 responses must not include a message body.
+*/
+
+const http = require('http');
+const max_requests=Number(process.argv[2]) || 10
+const options = {
+    port: 8080,
+    header:{'If-Unmodified-Since': Date()}
+};
+
+let time0
+let requests=0
+
+/* ignores server response */ 
+function request(){
+    let req=http.request(options)
+    req.end(() => {
+	requests++
+	if(requests<max_requests)
+	    setTimeout(request,0)
+	else {
+	    let time=(Date.now()-time0)/1000
+	    console.log(`Requests: ${requests} Time (sec):${time} RPS: ${max_requests/time}`)
+	    process.exit()
+	}
+    })
+    req.on('error', e => console.error(e.message))
+}
+
+time0=Date.now()
+request()
